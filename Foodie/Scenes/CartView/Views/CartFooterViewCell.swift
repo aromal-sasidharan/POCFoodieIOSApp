5f564a3ea2b4e330003662ded2364934dc99a98d
mahesh <mahesh.varadara@accenture.com>
2021-03-21 01:16:19 +0800

    #added footerview

---
+++ Foodie/Scenes/CartView/Views/CartFooterViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  PaymentFooterViewCell.swift
+//  Foodie
+//
+//  Created by mahesh.varadara on 20/3/21.
+//
+
+import UIKit
+
+class CartFooterViewCell: UITableViewHeaderFooterView {
+    
+//    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bgCardView: UIView!
+    @IBOutlet weak var labelSGST: UILabel!
+    
+    @IBOutlet weak var labelTotal: UILabel!
+    @IBOutlet weak var labelCGST: UILabel!
+    @IBOutlet weak var labelSubTotal: UILabel!
+  
+    class var identifier: String {
+        String(describing: Self.self)
+    }
+    
+    // Only override draw() if you perform custom drawing.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        bgCardView.layer.shadowColor = UIColor.lightGray.cgColor
+        bgCardView.layer.shadowRadius = 4.0
+        bgCardView.layer.cornerRadius = 10.0
+        bgCardView.layer.shadowOpacity = 1.0;
+        bgCardView.layer.shadowOffset = CGSize(width: 1, height: 1)
+        self.backgroundColor = .clear
+//        if UIDevice.current.hasNotch {
+//            bottomConstraint.constant = -35
+//        } else {
+//            bottomConstraint.constant = 0
+//        }
+    }
+}
+
+
